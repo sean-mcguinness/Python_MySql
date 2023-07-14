702fba01f55cdd167ef7dbeeb6802b43d17a6fa6
sean-mcguinness <sean.mcguinness97@gmail.com>
2023-07-14 15:56:46 +0200

    Added split_street_houseNr and cleaned up some code bits

--- migration.py
+++ migration.py
@@ -3,26 +3,27 @@
 # 2023
 
 # Imports
-import getpass
-import mysql.connector
+import getpass                                  # Passworteingabe im interaktiven Modus
+import mysql.connector                          # MySql Schnittstelle
 
-### Fuer Manuelles Setup ###
+### Fuer manuelles Setup ###
 # Connection-Setup
-man_db_host = None                          # Host eintragen (String) (default = localhost)
-man_db_name = None                          # Name der DB eintragen (String)
-man_db_user = None                          # Benutzername der DB eintragen (String)
-man_db_password = None                      # Passwort des DB-Benutzers eintragen (String)
-man_db_auth_plugin = None                   # Auth Plugin eintragen (String) (default = mysql_native_password)
+man_db_host = "localhost"                       # Host eintragen (String) (default = localhost)
+man_db_name = None                              # Name der DB eintragen (String)
+man_db_user = None                              # Benutzername der DB eintragen (String)
+man_db_password = None                          # Passwort des DB-Benutzers eintragen (String)
+man_db_auth_plugin = "mysql_native_password"    # Auth Plugin eintragen (String) (default = mysql_native_password)
 
 # DB-Param-Setup
-man_source_table = None                     # Quelle (Tabellename) eintragen (String)
-man_source_attribute = None                 # Quellattribut (Attribut) eintragen (String)
-man_sep_choice = None                       # Separator nachdem die Daten geteilt werden eintragen (String) (default = " ")
-man_destination_houseNr_attribute = None    # Zielattribut der Hausnummer eintragen (String)
+man_source_table = None                         # Quelle (Tabellename) eintragen (String)
+man_source_attribute = None                     # Quellattribut (Attribut) eintragen (String)
+man_sep_choice = " "                            # Separator nachdem die Daten geteilt werden eintragen (String) (default = " ")
+man_destination_houseNr_attribute = None        # Zielattribut der Hausnummer eintragen (String)
 
-man_destination_table = None                # Optional: Zieltabelle Eintragen (String)
-man_destination_street_attribute = None     # Optional: Zielattribut der Strasse eintragen (String) (default = source_table)
+man_destination_table = None                    # Optional: Zieltabelle Eintragen (String)
+man_destination_street_attribute = None         # Optional: Zielattribut der Strasse eintragen (String) (default = source_table)
 
+# Dictionary wird beim Funktionsaufruf entpackt -> Named Arguments
 dict_manual_edit = {
                     "db_name": man_db_name,
                     "db_user": man_db_user,
@@ -33,33 +34,40 @@ dict_manual_edit = {
                     }
 
 
-def menu(dict_manual_edit):
+def main(dict_manual_edit):
     do_menu_loop = True
 
     while do_menu_loop:
         answer = input("Skript manuell bearbeitet? [J/N] ")
 
-        if answer.upper == "J":
+        if answer.upper() == 'J':
             do_menu_loop = False
 
+            # Prueft die Manuellen anpassungen im Skript (Sind alle Pflichtvariablen definiert?)
             def check_params():
                 for variable in dict_manual_edit:
                     if variable is None:
-                        print(f"Die Definition von {variable} Fehlt!")
+                        print(f"Die Definition von {variable} fehlt!")
                         exit
 
             check_params()
             db_connection = connect_db(**dict_manual_edit)
-            split_street_houseNr_in_db(db_connection)
+            split_street_houseNr_in_db(db_connection, **dict_manual_edit)
 
-        elif answer.upper == "N":
+        elif answer.upper() == 'N':
             do_menu_loop = False
-            db_connection = interactive_edit()
+            ia_dict = interactive_edit()
+            db_connection = connect_db(ia_dict["db_name"], ia_dict["db_user"],
+                                       ia_dict["db_password"], ia_dict["hostname"],
+                                       ia_dict["db_auth_plugin"])
+            
+            split_street_houseNr_in_db(db_connection, ia_dict["source_table"],
+                                       ia_dict["source_attribute"], ia_dict["destination_table"],
+                                       ia_dict["destination_street_attribute"],
+                                       ia_dict["destination_houseNr_attribute"])
 
         else:
             print("Ungültige Eingabe!")
-
-    connect_db(db_connection)
 
     # Commitet die Aenderungen
     db_connection.commit()
@@ -70,40 +78,45 @@ def interactive_edit():
     ia_db_host = input("Hostname (default = localhost): ")
     ia_db_name = input("Name der Datenbank: ")
     ia_db_user = input("Benutzername: ")
-    ia_db_password = getpass()
+    ia_db_password = getpass.getpass("Passwort: ")
     ia_db_auth_plugin = input("DB Auth Plugin (default = mysql_native_password): ")
     ia_source_table = input("Quelle (Tabelle): ")
     ia_source_attribute = input("Quelle (Attribut): ")
     ia_destination_houseNr_attribute = input("Ziel (Attribut): ")
 
-    dict_interactive_edit = {
+    dict_ia_edit = {
+                                "hostname": "localhost",
                                 "db_name": ia_db_name,
                                 "db_user": ia_db_user,
                                 "db_password": ia_db_password,
+                                "db_auth_plugin": "mysql_native_password",
                                 "source_table": ia_source_table,
                                 "source_attribute": ia_source_attribute,
-                                "destination_houseNr_attribute": ia_destination_houseNr_attribute
+                                "destination_houseNr_attribute": ia_destination_houseNr_attribute,
+                                "destination_table": ia_source_table,
+                                "destination_street_attribute": ia_source_attribute,
+                                "sep_choice": None
                                 }
 
     if ia_db_host != "":
-        dict_interactive_edit["hostname"] = ia_db_host
+        dict_ia_edit["hostname"] = ia_db_host
 
     if ia_db_auth_plugin != "":
-        dict_interactive_edit["db_auth_plugin"] = ia_db_auth_plugin
+        dict_ia_edit["db_auth_plugin"] = ia_db_auth_plugin
 
     ask_use_optional_args = input("Optionale Argumente verwenden? (Andere Zieltabelle, Zielattribut für Strasse) [J/N] ")
 
-    if ask_use_optional_args.upper == "J":
+    if ask_use_optional_args.upper() == "J":
         do_loop1 = True
         while do_loop1:
             ask_use_other_dest_tbl = input("Andere Zieltabelle? [J/N]? ")
 
-            if ask_use_other_dest_tbl.upper == "J":
+            if ask_use_other_dest_tbl.upper() == "J":
                 do_loop1 = False
                 ia_destination_table = input("Zieltabelle: ")
-                dict_interactive_edit["destination_table"] = ia_destination_table
+                dict_ia_edit["destination_table"] = ia_destination_table
 
-            elif ask_use_other_dest_tbl.upper == "N":
+            elif ask_use_other_dest_tbl.upper() == "N":
                 do_loop1 = False
 
             else:
@@ -113,18 +126,31 @@ def interactive_edit():
 
         do_loop2 = True
         while do_loop2:
-            if ask_use_other_str_dest.upper == "J":
+            if ask_use_other_str_dest.upper() == "J":
                 do_loop2 = False
                 ia_destination_street_attribute = input("Zielattribut für Strasse: ")
-                dict_interactive_edit["destination_street_attribute"] = ia_destination_street_attribute
+                dict_ia_edit["destination_street_attribute"] = ia_destination_street_attribute
 
-            elif ask_use_other_str_dest.upper == "N":
+            elif ask_use_other_str_dest.upper() == "N":
                 do_loop2 = False
 
             else:
                 print("Ungültige Eingabe!")
+        ask_use_separator = input("Anderen Separator als default verwenden? (default = " ") [J/N]")
 
-    return dict_interactive_edit
+        do_loop3 = True
+        while do_loop3:
+            if ask_use_separator.upper() == "J":
+                do_loop3 = False
+                ia_sep_choice = input("Separator: ")
+                dict_ia_edit["sep_choice"] = ia_sep_choice
+            elif ask_use_separator.upper() == "N":
+                do_loop3 = False
+                pass
+            else:
+                print("Ungültige Eingabe!")
+
+    return dict_ia_edit
 
 
 def connect_db(db_name, db_user, db_password, db_host="localhost", db_auth_plugin="mysql_native_password"):
@@ -142,14 +168,14 @@ def connect_db(db_name, db_user, db_password, db_host="localhost", db_auth_plugi
     return db_connection
 
 
-# SELECT Statement um die benoetigten Daten aus der Tabelle zu ziehen
-stm_select_tbl = f"SELECT {source_attribute} FROM {source_table};"
-print(stm_select_tbl)
+def split_street_houseNr_in_db(db_connection,
+                               source_table, source_attribute,
+                               destination_table=None, destination_street_attribute=None, destination_houseNr_attribute=None,
+                               sep_choice=" "):
 
-
-def split_street_houseNr_in_db(db_connection, source_table, source_attribute,
-                               destination_table=None, destination_street_attribute=None,
-                               destination_houseNr_attribute=None, sep_choice=" "):
+    # SELECT Statement um die benoetigten Daten aus der Tabelle zu ziehen
+    stm_select_tbl = f"SELECT {source_attribute} FROM {source_table};"
+    print(stm_select_tbl)
 
     # Befehle durch mysql.connector an Datenbank senden und ausfuehren
     mein_cursor = db_connection.cursor(dictionary=True)
@@ -157,25 +183,38 @@ def split_street_houseNr_in_db(db_connection, source_table, source_attribute,
     mein_resultat = mein_cursor.fetchall()
 
     # Liste wird mit den benoetigten Daten gefuellt
-    splitAddr = []
+    street_houseNr = []
 
     # Fuellt Liste mit Daten
     for i in range(len(mein_resultat)):
-        splitAddr += mein_resultat[i][source_attribute].rsplit(sep_choice, 1)
+        # TODO Anzahl splits?
+        street_houseNr += mein_resultat[i][source_attribute].rsplit(" " if sep_choice is None else sep_choice, 1)
 
-    if destination_table is None and destination_street_attribute is None:
-        # Generiert UPDATE Statements für jeden Datensatz
-        n = 2
-        for x in range(len(mein_resultat)):
+    # Teilt Daten in 2 Listen
+    dict_split_street_houseNr = split_street_houseNr_in_string(street_houseNr)
 
-            if x == 0:
-                stm_update = f"UPDATE {source_table} SET {source_attribute}= \
-                \"{splitAddr[x]}\", {destination_houseNr_attribute}=\"{splitAddr[x+1]}\" WHERE id={x+1};"
+    # Generiert UPDATE Statements für jeden Datensatz
+    for x in range(0, len(mein_resultat)):
 
-            else:
-                stm_update = f"UPDATE {source_table} SET {source_attribute}= \
-                \"{splitAddr[n]}\", {destination_houseNr_attribute}=\"{splitAddr[n+1]}\" WHERE id={x+1};"
-                n += 2
-            print(f"Wird ausgeführt: {stm_update}")
+        stm_update = f"UPDATE {destination_table} \
+                        SET {destination_street_attribute}= \
+                        \"{dict_split_street_houseNr['streets'][x]}\", \
+                        {destination_houseNr_attribute}= \"{dict_split_street_houseNr['houseNrs'][x]}\" \
+                        WHERE id={x+1};"
 
-            mein_cursor.execute(stm_update)
+        print(f"Wird ausgeführt: {stm_update}")
+
+        mein_cursor.execute(stm_update)
+
+
+def split_street_houseNr_in_string(street_houseNr):
+    street = [street_houseNr[index] for index in range(0, len(street_houseNr), 2)]
+    houseNr = [street_houseNr[index] for index in range(1, len(street_houseNr), 2)]
+
+    dict_split_street_houseNr = {"streets": street, "houseNrs": houseNr}
+
+    return dict_split_street_houseNr
+
+
+# Startet das Skript
+main(dict_manual_edit)
